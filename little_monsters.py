fbc2e0129b7005fd96b175bc19a6ba5b0205191c
Gaël <gael@gael.com>
2022-02-13 11:59:25 +0100

    Added a weakness list for monsters to build weakness logic

--- little_monsters.py
+++ little_monsters.py
@@ -5,6 +5,9 @@ class Player:
         self.id = Player.player_id
         self.name = input_name
         self.team = input_team
+        self.active_monster = None
+        self.money = 100
+        self.ko_monsters = None
     
     def __repr__(self):
         repr_string = "Player " + str(self.id) + "'s name is " + self.name + " and they have the following monsters in their team: "
@@ -16,16 +19,28 @@ class Player:
         return repr_string
 
 class Monster:
-    
+    weaknesses = {'Fire' : 'Water', 'Water' : 'Plant', 'Plant' : 'Fire'}
+
     def __init__(self, name, type, level = 5):
         self.name = name
         self.type = type
         self.level = level
         self.hp = int(self.level * 10)
         self.is_ko = False
-        self.is_affected = False 
+        self.is_affected = False
+        self.is_active = False
+        self.weakness = Monster.weaknesses[self.type]
+    
+    def __repr__(self):
+        return self.name + ' is a ' + self.type + ' monster and is currently level ' + str(self.level) + ', its weakness is : ' + self.weakness
 
+a = ('Charmander', 'Fire')
+b = ('Squirtle', 'Water')
+c = ('Bulbasaur', 'Plant')
 
 #player_one = Player('Gaël', ['Charmander', 'Bulbasaur'])
 #player_two = Player('Théo', ['Squirtle', 'Buterfree'])
 #print(player_two)
+
+charmander = Monster('Charmander', 'Fire')
+print(charmander)
